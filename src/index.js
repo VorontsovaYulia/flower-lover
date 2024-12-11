05d9e94d1eb6d6ef326acf3658fd56526b69ba37
VorontsovaYulia <vorontsovayulia89@gmail.com>
2024-12-11 02:40:32 +0200

    created js files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/hero';
+import './js/advantages';
+import './js/gallery';
+import './js/delivery';
+import './js/reviews';
+import './js/quick_order';
+import './js/footer';
